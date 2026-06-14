7607e3b73ede84fda7d77f26acbee08c28751372
Rana Hany <ranahany1003@gmail.com>
2026-06-14 14:06:12 +0300

    feat: enable API mode for users controller and skip CSRF

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   # GET /users
   def index
     @users = User.all
+    render json: @users
   end
 
   # GET /users/:id
   def show
     @user = User.find(params[:id])
+    render json: @user
   end
 
   # GET /users/new
@@ -18,9 +22,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      render json: @user, status: :created
     else
-      render :new
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
