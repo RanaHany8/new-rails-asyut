415d60704f81966c2abf42a3dc1ddeb537269531
Rana Hany <ranahany1003@gmail.com>
2026-06-14 14:06:24 +0300

    fix: add uniqueness validation for user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   validates :name, :dob, :email, :phone_number, presence: true
-  
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  
-  
   validates :email, uniqueness: true
+
+  has_many :posts, foreign_key: 'user_id'
 end
